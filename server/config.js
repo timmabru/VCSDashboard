3965fff1cacf6917067f4bd242da0aad209aa8c6
Mihai Abrudan <mihai.abrudan@flex.com>
2018-01-11 23:40:36 -0800

    [Extension] Update application settings

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
     // Start URL that browsing should start at.
-    startURL: 'http://timnt030/vcsvisualplan/',
+    startURL: 'http://timnt030/vcsvisualplan/WHStatus.aspx',
     // Start Method when there is no start URL.  This is fpr when we are doing an API integration,
     // and we don't actually need to load phantom (yet).
     // startMethod: "login.onStart",
